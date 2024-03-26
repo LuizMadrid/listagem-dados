03fd44fd28c53a2d62ba9bcee4d066e17b243ebf
Luiz Madrid <luizmadrid.dev@gmail.com>
2024-03-26 15:27:44 -0300

    feat: add initial components.

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,131 @@
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import { FileDown, MoreHorizontal, Plus, Search } from 'lucide-react';
+import { useSearchParams } from 'react-router-dom';
+
+import { Header } from './components/header';
+import { Pagination } from './components/pagination';
+import { Tabs } from './components/tabs';
+import { Button } from './components/ui/button';
+import { Control, Input } from './components/ui/input';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table';
+
+
+export interface TagResponse {
+  first: number
+  prev: number | null
+  next: number
+  last: number
+  pages: number
+  items: number
+  data: Tag[]
+}
+
+export interface Tag {
+  title: string
+  slug: string
+  amountOfVideos: number
+  id: string
+}
+
 export function App() {
-  return (
-    <h1>hello world</h1>
-  )
+	const [searchParams] = useSearchParams();
+
+	const page = searchParams.get('page') ? Number(searchParams.get('page')) : 1;
+	
+	const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+		queryKey: ['get-tags', page],
+		queryFn: async () => {
+			const response = await fetch(`http://localhost:3333/tags?_page=${page}&_per_page=10`);
+			const data = await response.json();
+
+			return data;
+		},
+		placeholderData: keepPreviousData,
+	});
+
+	if (isLoading) {
+		return;
+	}
+
+	return (
+		<div className='py-10 space-y-8'>
+			<div>
+				<Header />
+				<Tabs />
+			</div>
+			
+			<main className='max-w-6xl mx-auto space-y-5'>
+				<div className='flex items-center gap-x-3'>
+					<h1 className='text-xl font-bold'>Tags</h1>
+					<Button variant='primary'>
+						<Plus className='size-3' />
+						Create new
+					</Button>
+				</div>
+
+				<div className='flex items-center justify-between'>
+					<Input variant='filter'>
+						<Search className='size-3' />
+						<Control placeholder='Search tags' />
+					</Input>
+
+					<Button variant='default'>
+						<FileDown className='size-3' />
+						Export
+					</Button>
+				</div>
+
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead></TableHead>
+							
+							<TableHead>
+								Tags
+							</TableHead>
+
+							<TableHead>
+								Amount of videos
+							</TableHead>
+							
+							<TableHead></TableHead>
+						</TableRow>
+					</TableHeader>
+
+					<TableBody>
+						{tagsResponse?.data.map((tag) => (
+							<TableRow key={tag.id}>
+								<TableCell></TableCell>
+
+								<TableCell>
+									<div className='flex flex-col gap-y-0.5'>
+										<span className='font-medium'>{tag.title}</span>
+										<span className='text-xs text-zinc-500'>{tag.id}</span>
+									</div>
+								</TableCell>
+							
+								<TableCell className='text-zinc-300'>
+									{tag.amountOfVideos > 1 ? (
+										`${tag.amountOfVideos} videos`
+									) : (
+										`${tag.amountOfVideos} video`
+									)}
+								</TableCell>
+							
+								<TableCell className='text-right'>
+									<Button size='icon'>
+										<MoreHorizontal className='size-4' />
+									</Button>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+
+				{tagsResponse && (
+					<Pagination pages={tagsResponse?.pages} items={tagsResponse.items} page={page} />
+				)}
+			</main>
+		</div>
+	);
 }
--- src/components/button.tsx
+++
@@ -1,9 +0,0 @@
-import { Plus } from 'lucide-react';
-
-export const Button = () => {
-	return (
-		<button className="w-10 m-10 mx-12 rounded-md shadow bg-slate-300">
-			<Plus className="w-6 h-6 text-slate-800" />
-		</button>
-	);
-};
